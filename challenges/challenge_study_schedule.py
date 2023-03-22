17aec6bf40137703f4f273bb9c692917169d3a3f
BrenoOPrado <breno.o.prado@hotmail.com>
2023-03-22 15:29:55 -0300

    desenvolvida função iteravel que verifica quantos alunos estavam estudando no momento especificado :turtle:

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    pass
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+
+        if target_time >= period[0] and target_time <= period[1]:
+            count += 1
+
+    return count
