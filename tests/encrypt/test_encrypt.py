c73777f089aee5d5d953a8c6ef7bf83ab336ab70
BrenoOPrado <breno.o.prado@hotmail.com>
2023-03-22 17:11:01 -0300

    testes para a função encrypt_message implementados :turtle:

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('exemplo1teste', 7) == 'olpmexe_etset1'
+    assert encrypt_message('exemplo1teste', 8) == 'etset_1olpmexe'
+    assert encrypt_message('teste', 9) == 'etset'
+    assert encrypt_message('teste', -1) == 'etset'
+    with pytest.raises(TypeError):
+        encrypt_message(3, 'error_test')
